ec761c64ae3891657132835d74ed904cb9c24a27
Gregori Faroux <gregori.faroux@catalyticds.com>
2018-08-15 23:20:43 -0400

    added science daily

--- generatedata.py
+++ generatedata.py
@@ -14,7 +14,6 @@ warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 #
 import numpy as np
-import feedparser
 from io import StringIO
 from pprint import pprint
 from nltk.stem.wordnet import WordNetLemmatizer
@@ -50,7 +49,13 @@ def text_from_html(body):
     visible_texts = filter(tag_visible, texts)  
     return u" ".join(t.strip() for t in visible_texts)
 
-def get_documents(list, url):
+def sciencedaily_text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    for div in soup.find_all('div', id='text'):
+      return(div.get_text())
+    return ""
+
+def get_google_documents(list, url):
   """Download a Google RSS feed links content to populate the list """
   d = feedparser.parse(url)
   for entry in d['entries']:
@@ -59,6 +64,20 @@ def get_documents(list, url):
       print('  ' + entry.title)
       print("")
       list.append(text_from_html(page))
+    except urllib.error.HTTPError as e:
+      print(e.reason)
+    except urllib.error.URLError as e:
+      print(e.reason)
+
+def get_sciencedaily_documents(list, url):
+  """Download Science Daily RSS feed links content to populate the list """
+  d = feedparser.parse(url)
+  for entry in d['entries']:
+    try:
+      page = urllib.request.urlopen(urllib.request.Request(entry.link, data=None, headers={ 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}))
+      print('  ' + entry.title)
+      print("")
+      list.append(sciencedaily_text_from_html(page))
     except urllib.error.HTTPError as e:
       print(e.reason)
     except urllib.error.URLError as e:
@@ -105,10 +124,19 @@ def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
 
 print('1. Get content')
 documents = []
-with open('words.txt') as f:
+
+#Science Daily
+with open('rss_science.txt') as f:
+   for url in f:
+       print(url)
+       get_sciencedaily_documents(documents, url)
+
+
+#Google News
+with open('rss_google_words.txt') as f:
    for word in f:
        print(word)
-       get_documents(documents, 'https://news.google.com/news/rss/search/section/q/' + word + '?ned=us&gl=US&hl=en')
+       get_google_documents(documents, 'https://news.google.com/news/rss/search/section/q/' + word + '?ned=us&gl=US&hl=en')
 
 print('Number of documents ' + str(len(documents)))
 
