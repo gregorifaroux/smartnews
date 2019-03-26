441d18a80d289fc248444a16956b4fc5b0343813
gregorifaroux <gregori.faroux@catalyticds.com>
2019-03-26 11:56:27 -0400

    add news API key

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 ## NLP on RSS news feed
 
-# python3 newsapi.py
+Collect news feeds from different data sources (Google News, The BBC, etc) and apply machine learning algorithms on them to automatically classify content and extract hidden topics.
+
+## To run
+
+```python
+> python3 newsapi.py
+```
 
 ## Setup
 
-sh setup.sh
+```bash
+> sh setup.sh
+```
+
+If CERT errors during the instal, go to Finder, search for Python 3.x. It will appear under Application folder. Expand the Python 3.6 folder. Then install certificates using "Install Certificates.command".
--- newsapi.py
+++ newsapi.py
@@ -1,10 +1,12 @@
 # import packages
 from __future__ import print_function
+import sys
 import requests
 import pandas as pd
 from datetime import datetime
 from tqdm import tqdm
 from matplotlib import pyplot as plt
+from pathlib import Path
 
 
 def getSources(category: str, lang: str):
@@ -15,14 +17,22 @@ def getSources(category: str, lang: str):
         sources.append(source["id"])
     return sources
 
+
 def mapping():
     d = {}
-    response = requests.get('https://newsapi.org/v1/sources?language=en')
+    response = requests.get("https://newsapi.org/v1/sources?language=en")
     response = response.json()
-    for s in response['sources']:
-        d[s['id']] = s['category']
+    for s in response["sources"]:
+        d[s["id"]] = s["category"]
     return d
 
+
+# Check if there is an API key
+config = Path("./newsapi.key")
+if not config.is_file():
+    sys.exit("newsapi.org requires a key: please create a key 'newsapi.key'")
+key = config.read_text()
+print(f"newsapi.org key is: {key}")
 
 # Sources in English
 sources = getSources("business", "en")
@@ -34,5 +44,5 @@ print("number of sources :", len(sources))
 # Categories
 m = mapping()
 for source in sources:
-    print(f'{source}: {m[source]}')
+    print(f"{source}: {m[source]}")
 
