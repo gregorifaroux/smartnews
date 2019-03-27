2e55c553f1545fc7a018cc33f27e5faffd747a02
Gregori Faroux <gregori.faroux@catalyticds.com>
2019-03-27 00:30:50 -0400

    lda

--- newsapi_3_data_discovery.py
+++ newsapi_3_data_discovery.py
@@ -20,11 +20,23 @@ from collections import Counter
 import re
 import numpy as np
 
+import matplotlib
+
+matplotlib.use("TkAgg", warn=True, force=False)
+print("Using: " + matplotlib.get_backend())
 import matplotlib.pyplot as plt
+
 
 #
 from sklearn.feature_extraction.text import TfidfVectorizer
 from wordcloud import WordCloud
+
+# Perform LDA
+
+import gensim
+import gensim.corpora as corpora
+from gensim import matutils
+from gensim.models import CoherenceModel
 
 
 """
@@ -87,6 +99,75 @@ def plot_word_cloud(terms):
     plt.show()
 
 
+def LDA_model(corpus, id2word, num_topics, passes=1):
+    return gensim.models.ldamodel.LdaModel(
+        corpus=corpus,
+        id2word=id2word,
+        num_topics=num_topics,
+        random_state=100,
+        eval_every=10,
+        chunksize=2000,
+        passes=passes,
+        per_word_topics=True,
+    )
+
+
+def compute_coherence(model, aux, id2word):
+    coherence = CoherenceModel(
+        model=model,
+        texts=aux["tokens_bigram"].values,
+        dictionary=id2word,
+        coherence="c_v",
+    )
+    return coherence.get_coherence()
+
+
+def display_topics(model):
+    topics = model.show_topics(
+        num_topics=model.num_topics, formatted=False, num_words=10
+    )
+    topics = map(lambda c: map(lambda cc: cc[0], c[1]), topics)
+    df = pd.DataFrame(topics)
+    df.index = ["topic_{0}".format(i) for i in range(model.num_topics)]
+    df.columns = ["keyword_{0}".format(i) for i in range(1, 10 + 1)]
+    return df
+
+
+def explore_models(df, rg=range(5, 25)):
+    id2word = corpora.Dictionary(df["tokens_bigram"])
+    texts = df["tokens_bigram"].values
+    corpus = [id2word.doc2bow(text) for text in texts]
+    models = []
+    coherences = []
+
+    for num_topics in rg:
+        lda_model = LDA_model(corpus, id2word, num_topics, passes=5)
+        models.append(lda_model)
+        coherence = compute_coherence(lda_model, df, id2word)
+        coherences.append(coherence)
+
+    fig = plt.figure(figsize=(15, 5))
+    plt.title("Choosing the optimal number of topics")
+    plt.xlabel("Number of topics")
+    plt.ylabel("Coherence")
+    plt.grid(True)
+    plt.plot(rg, coherences)
+
+    return coherences, models
+
+
+def get_document_topic_matrix(corpus, best_model, num_topics):
+    matrix = []
+    for row in corpus:
+        output = np.zeros(num_topics)
+        doc_proba = best_model[row][0]
+        for doc, proba in doc_proba:
+            output[doc] = proba
+        matrix.append(output)
+    matrix = np.array(matrix)
+    return matrix
+
+
 def main():
 
     print("Data discovery ... ")
@@ -116,10 +197,10 @@ def main():
     #    plt.show()
 
     print("Text processing : tokenization")
-    data["tokens"] = data["content"].map(lambda d: tokenizer(d))
+    data["tokens"] = data["description"].map(lambda d: tokenizer(d))
 
-    for descripition, tokens in zip(data["content"].head(5), data["tokens"].head(5)):
-        print("description:", descripition)
+    for description, tokens in zip(data["description"].head(5), data["tokens"].head(5)):
+        print("description:", description)
         print("tokens:", tokens)
         print()
 
@@ -140,8 +221,32 @@ def main():
     tfidf = pd.DataFrame(columns=["tfidf"]).from_dict(dict(tfidf), orient="index")
     tfidf.columns = ["tfidf"]
 
-    plot_word_cloud(tfidf.sort_values(by=["tfidf"], ascending=True).head(40))
-    plot_word_cloud(tfidf.sort_values(by=["tfidf"], ascending=False).head(40))
+    #   plot_word_cloud(tfidf.sort_values(by=["tfidf"], ascending=True).head(40))
+    #  plot_word_cloud(tfidf.sort_values(by=["tfidf"], ascending=False).head(40))
+
+    print(" topic modeling - LDA")
+    aux = data.copy()
+
+    bigram = gensim.models.Phrases(aux["tokens"], min_count=5, threshold=100)
+    bigram_mod = gensim.models.phrases.Phraser(bigram)
+    aux["tokens_bigram"] = aux["tokens"].map(lambda tokens: bigram_mod[tokens])
+    id2word = corpora.Dictionary(aux["tokens_bigram"])
+    texts = aux["tokens_bigram"].values
+    corpus = [id2word.doc2bow(text) for text in texts]
+    coherences, models = explore_models(aux, rg=range(5, 85, 5))
+    best_model = LDA_model(corpus, id2word, num_topics=55, passes=5)
+    print(display_topics(model=best_model))
+
+    matrix = get_document_topic_matrix(corpus, best_model, best_model.num_topics)
+    doc_topic = best_model.get_document_topics(corpus)
+    lda_keys = []
+    for i, desc in enumerate(data["description"]):
+        lda_keys.append(np.argmax(matrix[i, :]))
+        print(desc)
+        print(best_model.show_topic(np.argmax(matrix[i, :])))
+        print()
+    #    plt.show()
+
     print("Done")
 
 
